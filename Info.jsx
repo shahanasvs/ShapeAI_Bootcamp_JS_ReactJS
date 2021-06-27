1730d2d1897c189dcefd11f6995ee815692be45c
shahanasvs <50138590+shahanasvs@users.noreply.github.com>
2021-06-28 00:58:48 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1> Vegetables </h1>
+      <p>Vegetables are some of the most important foods we need to consume regularly. 
+        Depending on which part of the plant is consumed, the food in the vegetables category is divided into a number of subcategories. There are root vegetables, bulb vegetables, stem, leafy, and podded vegetables. 
+      </p>
+    </div>
+  );
+}
+export default Info;
---
+++ Info1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1> Fruits </h1>
+      <p>
+      Foods in the fruits category are not just delicious but they are also healthy. 
+      Different types of fruit are common in all food cultures. 
+      We can divide the foods in the fruits category into various categories such as tropical, citrus, drupes (fruit with a single hard stone), berries, pomes (such as apples), and pepos (such as melons).
+      </p>
+    </div>
+  );
+}
+export default Info;
